f26f65429cbb521b2332a912ded1642486db9d66
Asiya <asya.asha@gmail.com>
2023-10-22 09:28:20 -0400

    add whitelisted

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -5,9 +5,11 @@ const base = 'http://0.0.0.0:5555';
 
 export async function load({ fetch }) {
 	const response = await fetch(`${base}/ledgers`);
+	const resWhitelisted = await fetch(`${base}/whitelisted`);
 	const data = await response.json();
+	const whitelisted = await resWhitelisted.json();
 
 	return data?.ledgers
-		? { data: Object.keys(data?.ledgers).map((item) => data?.ledgers[item]) }
-		: { data: [] };
+		? { ledgers: Object.keys(data?.ledgers).map((item) => data?.ledgers[item]), whitelisted }
+		: { ledgers: [], whitelisted };
 }
